859506272109c26f1a00603093aa30288dbd3066
Vadim <vadimyanchenko64@gmail.com>
2024-10-23 16:38:02 +0400

    fix: Исправлена ошибка в формировании таблицы

--- src/models/reader.py
+++ src/models/reader.py
@@ -6,8 +6,9 @@ from  src.db.postgres.database import Base
 class Readers(Base):
     __tablename__ = "readers"
 
-    id_author: Mapped[int] = mapped_column(primary_key=True)
+    reader_ticket: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
-    country: Mapped[str] = mapped_column(nullable=False)
-    birthday: Mapped[Date] = mapped_column(Date)
-
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    phone_number: Mapped[str] = mapped_column(nullable=False, unique=True)
+    passport: Mapped[str] = mapped_column(nullable=False, unique=True)
+    created_date: Mapped[Date] = mapped_column(Date, nullable=False)
